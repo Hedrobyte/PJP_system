0d2ae5835954e6b58f622a7dccc0d8abeda595ef
Hedrobyte <pedrohqmalves@gmail.com>
2022-08-12 13:09:47 -0300

    teste

--- teste.java
+++ teste.java
@@ -2,5 +2,6 @@ public class teste{
 
      public static void main(String[] args) throws Exception {
         System.out.println("Olá Mundo!!!");
+        System.out.println("Olá Mundo 12!!!");
     }
 }
