77a81169e672b0ace73e84a7e454dded462ca87e
Pedro Henrique dos Santos Mateus <pedrobox10@live.com>
2022-08-16 11:04:53 -0300

    deletado teste

--- teste.java
+++
@@ -1,7 +0,0 @@
-public class teste{
-
-     public static void main(String[] args) throws Exception {
-        System.out.println("Olá Mundo!!!");
-        System.out.println("Olá Mundo 12!!!");
-    }
-}
